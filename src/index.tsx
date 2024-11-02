b35b9e5a7d5413c529c805dd2a8687f70447658b
Danil <kraskafet@gmail.com>
2024-11-02 15:02:03 +0500

    настроил контекст

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TaskStore from "./store/TaskStore";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+interface State {
+    taskStore: TaskStore
+}
+export const taskStore = new TaskStore()
+export const Context = createContext<State>({
+    taskStore
+})
+
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+      taskStore
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
 
